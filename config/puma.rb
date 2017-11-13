fa5021bb6ce18959e1e592ef3cf5e83df86bd28b
wedev17 <wedev17@gmail.com>
2017-11-13 16:35:08 -0600

    on_worker_boot do

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ environment ENV['RACK_ENV'] || 'development'
 preload_app!
 
 
-worker specific setup
+on_worker_boot do
 ActiveSupport.on_load(:active_record) do
 ActiveRecord::Base.establish_connection
 end
